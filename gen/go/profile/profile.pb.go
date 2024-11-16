1640523e817e59f63dcabacead58593bab373045
your Name <qwertyshurazh@gmail.com>
2024-11-16 23:07:57 +0300

    update service name

--- gen/go/profile/profile.pb.go
+++ gen/go/profile/profile.pb.go
@@ -2,9 +2,9 @@
 // versions:
 // 	protoc-gen-go v1.28.1
 // 	protoc        v5.27.1
-// source: auth.proto
+// source: profile.proto
 
-package authv1
+package profilev1
 
 import (
 	protoreflect "google.golang.org/protobuf/reflect/protoreflect"
@@ -31,7 +31,7 @@ type GetVkIDRequest struct {
 func (x *GetVkIDRequest) Reset() {
 	*x = GetVkIDRequest{}
 	if protoimpl.UnsafeEnabled {
-		mi := &file_auth_proto_msgTypes[0]
+		mi := &file_profile_proto_msgTypes[0]
 		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
 		ms.StoreMessageInfo(mi)
 	}
@@ -44,7 +44,7 @@ func (x *GetVkIDRequest) String() string {
 func (*GetVkIDRequest) ProtoMessage() {}
 
 func (x *GetVkIDRequest) ProtoReflect() protoreflect.Message {
-	mi := &file_auth_proto_msgTypes[0]
+	mi := &file_profile_proto_msgTypes[0]
 	if protoimpl.UnsafeEnabled && x != nil {
 		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
 		if ms.LoadMessageInfo() == nil {
@@ -57,7 +57,7 @@ func (x *GetVkIDRequest) ProtoReflect() protoreflect.Message {
 
 // Deprecated: Use GetVkIDRequest.ProtoReflect.Descriptor instead.
 func (*GetVkIDRequest) Descriptor() ([]byte, []int) {
-	return file_auth_proto_rawDescGZIP(), []int{0}
+	return file_profile_proto_rawDescGZIP(), []int{0}
 }
 
 func (x *GetVkIDRequest) GetUserID() int64 {
@@ -78,7 +78,7 @@ type GetVkIDResponse struct {
 func (x *GetVkIDResponse) Reset() {
 	*x = GetVkIDResponse{}
 	if protoimpl.UnsafeEnabled {
-		mi := &file_auth_proto_msgTypes[1]
+		mi := &file_profile_proto_msgTypes[1]
 		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
 		ms.StoreMessageInfo(mi)
 	}
@@ -91,7 +91,7 @@ func (x *GetVkIDResponse) String() string {
 func (*GetVkIDResponse) ProtoMessage() {}
 
 func (x *GetVkIDResponse) ProtoReflect() protoreflect.Message {
-	mi := &file_auth_proto_msgTypes[1]
+	mi := &file_profile_proto_msgTypes[1]
 	if protoimpl.UnsafeEnabled && x != nil {
 		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
 		if ms.LoadMessageInfo() == nil {
@@ -104,7 +104,7 @@ func (x *GetVkIDResponse) ProtoReflect() protoreflect.Message {
 
 // Deprecated: Use GetVkIDResponse.ProtoReflect.Descriptor instead.
 func (*GetVkIDResponse) Descriptor() ([]byte, []int) {
-	return file_auth_proto_rawDescGZIP(), []int{1}
+	return file_profile_proto_rawDescGZIP(), []int{1}
 }
 
 func (x *GetVkIDResponse) GetVkID() int64 {
@@ -114,44 +114,46 @@ func (x *GetVkIDResponse) GetVkID() int64 {
 	return 0
 }
 
-var File_auth_proto protoreflect.FileDescriptor
+var File_profile_proto protoreflect.FileDescriptor
 
-var file_auth_proto_rawDesc = []byte{
-	0x0a, 0x0a, 0x61, 0x75, 0x74, 0x68, 0x2e, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x12, 0x04, 0x61, 0x75,
-	0x74, 0x68, 0x22, 0x28, 0x0a, 0x0e, 0x47, 0x65, 0x74, 0x56, 0x6b, 0x49, 0x44, 0x52, 0x65, 0x71,
-	0x75, 0x65, 0x73, 0x74, 0x12, 0x16, 0x0a, 0x06, 0x75, 0x73, 0x65, 0x72, 0x49, 0x44, 0x18, 0x01,
-	0x20, 0x01, 0x28, 0x03, 0x52, 0x06, 0x75, 0x73, 0x65, 0x72, 0x49, 0x44, 0x22, 0x25, 0x0a, 0x0f,
-	0x47, 0x65, 0x74, 0x56, 0x6b, 0x49, 0x44, 0x52, 0x65, 0x73, 0x70, 0x6f, 0x6e, 0x73, 0x65, 0x12,
-	0x12, 0x0a, 0x04, 0x76, 0x6b, 0x49, 0x44, 0x18, 0x01, 0x20, 0x01, 0x28, 0x03, 0x52, 0x04, 0x76,
-	0x6b, 0x49, 0x44, 0x32, 0x47, 0x0a, 0x0b, 0x41, 0x75, 0x74, 0x68, 0x53, 0x65, 0x72, 0x76, 0x69,
-	0x63, 0x65, 0x12, 0x38, 0x0a, 0x07, 0x47, 0x65, 0x74, 0x56, 0x6b, 0x49, 0x44, 0x12, 0x14, 0x2e,
-	0x61, 0x75, 0x74, 0x68, 0x2e, 0x47, 0x65, 0x74, 0x56, 0x6b, 0x49, 0x44, 0x52, 0x65, 0x71, 0x75,
-	0x65, 0x73, 0x74, 0x1a, 0x15, 0x2e, 0x61, 0x75, 0x74, 0x68, 0x2e, 0x47, 0x65, 0x74, 0x56, 0x6b,
-	0x49, 0x44, 0x52, 0x65, 0x73, 0x70, 0x6f, 0x6e, 0x73, 0x65, 0x22, 0x00, 0x42, 0x1a, 0x5a, 0x18,
-	0x56, 0x6d, 0x65, 0x73, 0x74, 0x65, 0x41, 0x70, 0x70, 0x2e, 0x61, 0x75, 0x74, 0x68, 0x2e, 0x76,
-	0x31, 0x3b, 0x61, 0x75, 0x74, 0x68, 0x76, 0x31, 0x62, 0x06, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x33,
+var file_profile_proto_rawDesc = []byte{
+	0x0a, 0x0d, 0x70, 0x72, 0x6f, 0x66, 0x69, 0x6c, 0x65, 0x2e, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x12,
+	0x07, 0x70, 0x72, 0x6f, 0x66, 0x69, 0x6c, 0x65, 0x22, 0x28, 0x0a, 0x0e, 0x47, 0x65, 0x74, 0x56,
+	0x6b, 0x49, 0x44, 0x52, 0x65, 0x71, 0x75, 0x65, 0x73, 0x74, 0x12, 0x16, 0x0a, 0x06, 0x75, 0x73,
+	0x65, 0x72, 0x49, 0x44, 0x18, 0x01, 0x20, 0x01, 0x28, 0x03, 0x52, 0x06, 0x75, 0x73, 0x65, 0x72,
+	0x49, 0x44, 0x22, 0x25, 0x0a, 0x0f, 0x47, 0x65, 0x74, 0x56, 0x6b, 0x49, 0x44, 0x52, 0x65, 0x73,
+	0x70, 0x6f, 0x6e, 0x73, 0x65, 0x12, 0x12, 0x0a, 0x04, 0x76, 0x6b, 0x49, 0x44, 0x18, 0x01, 0x20,
+	0x01, 0x28, 0x03, 0x52, 0x04, 0x76, 0x6b, 0x49, 0x44, 0x32, 0x50, 0x0a, 0x0e, 0x50, 0x72, 0x6f,
+	0x66, 0x69, 0x6c, 0x65, 0x53, 0x65, 0x72, 0x76, 0x69, 0x63, 0x65, 0x12, 0x3e, 0x0a, 0x07, 0x47,
+	0x65, 0x74, 0x56, 0x6b, 0x49, 0x44, 0x12, 0x17, 0x2e, 0x70, 0x72, 0x6f, 0x66, 0x69, 0x6c, 0x65,
+	0x2e, 0x47, 0x65, 0x74, 0x56, 0x6b, 0x49, 0x44, 0x52, 0x65, 0x71, 0x75, 0x65, 0x73, 0x74, 0x1a,
+	0x18, 0x2e, 0x70, 0x72, 0x6f, 0x66, 0x69, 0x6c, 0x65, 0x2e, 0x47, 0x65, 0x74, 0x56, 0x6b, 0x49,
+	0x44, 0x52, 0x65, 0x73, 0x70, 0x6f, 0x6e, 0x73, 0x65, 0x22, 0x00, 0x42, 0x20, 0x5a, 0x1e, 0x56,
+	0x6d, 0x65, 0x73, 0x74, 0x65, 0x41, 0x70, 0x70, 0x2e, 0x70, 0x72, 0x6f, 0x66, 0x69, 0x6c, 0x65,
+	0x2e, 0x76, 0x31, 0x3b, 0x70, 0x72, 0x6f, 0x66, 0x69, 0x6c, 0x65, 0x76, 0x31, 0x62, 0x06, 0x70,
+	0x72, 0x6f, 0x74, 0x6f, 0x33,
 }
 
 var (
-	file_auth_proto_rawDescOnce sync.Once
-	file_auth_proto_rawDescData = file_auth_proto_rawDesc
+	file_profile_proto_rawDescOnce sync.Once
+	file_profile_proto_rawDescData = file_profile_proto_rawDesc
 )
 
-func file_auth_proto_rawDescGZIP() []byte {
-	file_auth_proto_rawDescOnce.Do(func() {
-		file_auth_proto_rawDescData = protoimpl.X.CompressGZIP(file_auth_proto_rawDescData)
+func file_profile_proto_rawDescGZIP() []byte {
+	file_profile_proto_rawDescOnce.Do(func() {
+		file_profile_proto_rawDescData = protoimpl.X.CompressGZIP(file_profile_proto_rawDescData)
 	})
-	return file_auth_proto_rawDescData
+	return file_profile_proto_rawDescData
 }
 
-var file_auth_proto_msgTypes = make([]protoimpl.MessageInfo, 2)
-var file_auth_proto_goTypes = []interface{}{
-	(*GetVkIDRequest)(nil),  // 0: auth.GetVkIDRequest
-	(*GetVkIDResponse)(nil), // 1: auth.GetVkIDResponse
+var file_profile_proto_msgTypes = make([]protoimpl.MessageInfo, 2)
+var file_profile_proto_goTypes = []interface{}{
+	(*GetVkIDRequest)(nil),  // 0: profile.GetVkIDRequest
+	(*GetVkIDResponse)(nil), // 1: profile.GetVkIDResponse
 }
-var file_auth_proto_depIdxs = []int32{
-	0, // 0: auth.AuthService.GetVkID:input_type -> auth.GetVkIDRequest
-	1, // 1: auth.AuthService.GetVkID:output_type -> auth.GetVkIDResponse
+var file_profile_proto_depIdxs = []int32{
+	0, // 0: profile.ProfileService.GetVkID:input_type -> profile.GetVkIDRequest
+	1, // 1: profile.ProfileService.GetVkID:output_type -> profile.GetVkIDResponse
 	1, // [1:2] is the sub-list for method output_type
 	0, // [0:1] is the sub-list for method input_type
 	0, // [0:0] is the sub-list for extension type_name
@@ -159,13 +161,13 @@ var file_auth_proto_depIdxs = []int32{
 	0, // [0:0] is the sub-list for field type_name
 }
 
-func init() { file_auth_proto_init() }
-func file_auth_proto_init() {
-	if File_auth_proto != nil {
+func init() { file_profile_proto_init() }
+func file_profile_proto_init() {
+	if File_profile_proto != nil {
 		return
 	}
 	if !protoimpl.UnsafeEnabled {
-		file_auth_proto_msgTypes[0].Exporter = func(v interface{}, i int) interface{} {
+		file_profile_proto_msgTypes[0].Exporter = func(v interface{}, i int) interface{} {
 			switch v := v.(*GetVkIDRequest); i {
 			case 0:
 				return &v.state
@@ -177,7 +179,7 @@ func file_auth_proto_init() {
 				return nil
 			}
 		}
-		file_auth_proto_msgTypes[1].Exporter = func(v interface{}, i int) interface{} {
+		file_profile_proto_msgTypes[1].Exporter = func(v interface{}, i int) interface{} {
 			switch v := v.(*GetVkIDResponse); i {
 			case 0:
 				return &v.state
@@ -194,18 +196,18 @@ func file_auth_proto_init() {
 	out := protoimpl.TypeBuilder{
 		File: protoimpl.DescBuilder{
 			GoPackagePath: reflect.TypeOf(x{}).PkgPath(),
-			RawDescriptor: file_auth_proto_rawDesc,
+			RawDescriptor: file_profile_proto_rawDesc,
 			NumEnums:      0,
 			NumMessages:   2,
 			NumExtensions: 0,
 			NumServices:   1,
 		},
-		GoTypes:           file_auth_proto_goTypes,
-		DependencyIndexes: file_auth_proto_depIdxs,
-		MessageInfos:      file_auth_proto_msgTypes,
+		GoTypes:           file_profile_proto_goTypes,
+		DependencyIndexes: file_profile_proto_depIdxs,
+		MessageInfos:      file_profile_proto_msgTypes,
 	}.Build()
-	File_auth_proto = out.File
-	file_auth_proto_rawDesc = nil
-	file_auth_proto_goTypes = nil
-	file_auth_proto_depIdxs = nil
+	File_profile_proto = out.File
+	file_profile_proto_rawDesc = nil
+	file_profile_proto_goTypes = nil
+	file_profile_proto_depIdxs = nil
 }
